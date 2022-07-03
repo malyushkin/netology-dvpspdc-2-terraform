c1ce66486af4a68605262b78a7ea7d7b39709c70
Roman <malyushkinr@gmail.com>
2022-07-03 10:36:30 +0200

    Delete state.tf

--- state.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "romanmaliushkin-netology-terraform-state"
-    key            = "state/terraform.tfstate"
-    region         = "eu-west-3"
-    dynamodb_table = "terraform-lock"
-    encrypt        = true
-  }
-}
